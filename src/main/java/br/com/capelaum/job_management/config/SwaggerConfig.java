e9161c1a529466df6eb9b9041337461dff71589c
Luís Vinicius Capelletto <thecapellett@gmail.com>
2024-05-22 17:55:38 -0300

    refactor SwaggerConfig

--- src/main/java/br/com/capelaum/job_management/config/SwaggerConfig.java
+++ src/main/java/br/com/capelaum/job_management/config/SwaggerConfig.java
@@ -1,34 +1,35 @@
 package br.com.capelaum.job_management.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
+	@Bean
+	public OpenAPI openAPI() {
 
-        return new OpenAPI().info(new Info()
-                .title("Gestão de Vagas")
-                .description("API responsável pela gestão de vagas")
-                .version("1"))
-                .schemaRequirement("jwt_auth", createSecurityScheme());
-        // .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-        // .components(new Components().addSecuritySchemes("Bearer Authentication",
-        // createSecurityScheme()));
-    }
+		return new OpenAPI()
+				.info(new Info()
+						.title("Gestão de Vagas")
+						.description("API responsável pela gestão de vagas")
+						.version("1"))
+				.schemaRequirement("jwt_auth", createSecurityScheme()
+				);
+		// .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+		// .components(new Components().addSecuritySchemes("Bearer Authentication",
+		// createSecurityScheme()));
+	}
 
-    private SecurityScheme createSecurityScheme() {
-        return new SecurityScheme()
-                .name("jwt_auth")
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-    }
+	private SecurityScheme createSecurityScheme() {
+		return new SecurityScheme()
+				.name("jwt_auth")
+				.type(SecurityScheme.Type.HTTP)
+				.scheme("bearer")
+				.bearerFormat("JWT");
+	}
 
 }
