ed75de14b04af04c6fbc90181200b0afe1f35e2e
Luís Vinicius Capelletto <thecapellett@gmail.com>
2024-05-28 14:05:52 -0300

    update: ListAllJobsByFilterUseCase optional filter

--- src/main/java/br/com/capelaum/job_management/modules/candidate/useCases/ListAllJobsByFilterUseCase.java
+++ src/main/java/br/com/capelaum/job_management/modules/candidate/useCases/ListAllJobsByFilterUseCase.java
@@ -1,12 +1,14 @@
 package br.com.capelaum.job_management.modules.candidate.useCases;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.capelaum.job_management.modules.company.entities.JobEntity;
 import br.com.capelaum.job_management.modules.company.repositories.JobRepository;
+import org.springframework.util.StringUtils;
 
 @Service
 public class ListAllJobsByFilterUseCase {
@@ -15,7 +17,11 @@ public class ListAllJobsByFilterUseCase {
     JobRepository jobRepository;
 
     public List<JobEntity> execute(String filter) {
-        return this.jobRepository.findByDescriptionContainingIgnoreCase(filter);
+        if (StringUtils.hasText(filter)) {
+            return this.jobRepository.findByDescriptionContainingIgnoreCase(filter);
+        }
+
+        return jobRepository.findAll();
     }
 
 }
